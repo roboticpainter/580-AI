99980a63335b21feb325ede00053e0a779251cc1
Colby Bates <batesbobby.1@netzero.net>
2016-10-31 17:17:19 -0700

    Fixed everything and done experimenting with Git functions

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,1 +1,17 @@
-//stuff2
+/*
+ * Author: Colby Bates
+ * File: main.cpp
+ */
+
+
+#include "Ann.h"
+
+using namespace std;
+using std::cout;
+
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
